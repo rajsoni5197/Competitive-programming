6420c1eafefe385890574d10f70bd6ffb607a1c0
Raj soni <74926805+rajsoni5197@users.noreply.github.com>
2023-05-02 12:30:29 +0530

    Prime Test upto 1<= n <= 10^12
    This file is the way to find if a no is prime or not for large number like 10^16 .it is done by using segmented sieve approach .

---
+++ primetest.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+class Sieve
+{public:
+    int n;
+    bool* sieve;
+    Sieve(int n)
+    {
+        sieve = new bool[n+1];
+        sieve[0] = sieve[1]= 0;
+        for(int i= 2 ; i< n;i++ )
+        {
+            sieve[i] = 1; 
+        }
+        for(int i = 2 ;  i*i< n ; i++ )
+        {
+            if(sieve[i] == 1)
+            {
+                for(int j = i*i ; j<= n ; j+=i)
+                {
+                    sieve[j] = 0;
+                }
+            }
+        }
+    }
+    bool isPrime(int n)
+    {
+        return sieve[n];
+    }
+};
+void createSegSieve(vector<unsigned long long>& res, vector<int> &primes, unsigned long long l , unsigned long long r)
+{
+    res.clear();
+    bool segSieve[(r-l)+1];
+    for(int i = 0 ;i < (r-l)+1 ; i++)
+    {  segSieve[i] =1;
+     
+    }
+    for(auto pri : primes)
+    {
+        
+        unsigned long long firstFactor = (l/pri)*pri;
+        if(firstFactor<l) firstFactor += pri;
+        if(firstFactor< (unsigned long long)pri*pri) firstFactor =( unsigned long long)pri* pri;
+
+        for(unsigned long long i = firstFactor ; i<= r ; i+= pri)
+        {
+            segSieve[i-l] = 0;
+        }
+    }
+   
+    for(int i = 0 ; i< (r-l)+1 ; i++)
+    {
+        if(segSieve[i]) 
+        { 
+            res.push_back(i+l);
+        }
+    }
+
+}
+int main()
+{
+    Sieve soe(1e6);
+    vector<int>primes;
+    vector<unsigned long long>resPrime ;
+    unsigned long long no; 
+    for(int i = 2;i<= 1e6 ;i++)
+    {
+        if(soe.isPrime(i))
+        {
+            primes.push_back(i);
+        }
+    }
+    
+        cin>>no;
+        if(no==1){cout<<"NO"<<endl; return 0;}
+        createSegSieve(resPrime,primes,no,no);
+        if(resPrime.size()&&resPrime[0] == no) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    
+   
+    return 0;
+
+}
